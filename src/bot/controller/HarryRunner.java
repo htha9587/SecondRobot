abca43fda83743bed37474ff2b1bf01a40f23288
htha9587 <harrison.thacker69@gmail.com>
2015-12-16 12:10:23 -0700

    Version 0
    Built Controller and Runner.

---
+++ src/bot/controller/HarryRunner.java
@@ -0,0 +1,22 @@
+package bot.controller;
+
+/**
+ * 
+ * @author htha9587
+ * version 0.x 12-16-15
+ */
+
+public class HarryRunner 
+{
+/**
+ * 
+ * @param args
+ */
+
+	public static void main(String [] args)
+	{
+		RobotController myRobotController = new RobotController();
+		myRobotController.start();
+	}
+	
+	}
